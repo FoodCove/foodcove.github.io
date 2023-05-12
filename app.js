655fa9ec3c4e1e2b100106c3c9aebbeb146f98a1
Neel <131549658+FoodCove@users.noreply.github.com>
2023-05-12 19:42:56 -0400

    Added some breakfast options

--- app.js
+++ app.js
@@ -1,12 +1,71 @@
-const breakfastList = ["pancakes","Paratha","Poha","Idli","Dosa","Uttapam","Chilla","Cheela","Aloo Puri","Bread Pakora","Bread Toast","Samosa","Kachori","Chole Bhature","Poori Bhaji","Besan Cheela","Methi Paratha","Paneer Paratha","Egg Paratha","Aloo Paratha","Cheese Paratha","Bhatura","Kachori with Aloo Sabzi","Matar Kachori","Aloo Tikki","Chana Dal Paratha","Moong Dal Chilla","Sabudana Khichdi","Maggi"];
-const lunchList = ["Chana Masala","Rajma Masala","Aloo Gobi","Aloo Matar","Baingan Bharta","Bhindi Masala","Palak Paneer","Matar Paneer","Paneer Butter Masala","Kadai Paneer","Paneer Bhurji","Mushroom Masala","Malai Kofta","Vegetable Kofta Curry","Navratna Korma","Jeera Aloo","Dum Aloo","Masala Chaas","Punjabi Samosa","Chole Bhature","Pav Bhaji","Paneer Paratha","Aloo Paratha","Gobi Paratha","Methi Paratha","Missi Roti","Lachha Paratha","Dal Makhani","Tawa Pulao","Veg Pulao"];
-const dinnerList = ["Dal Makhani","Rajma","Chole","Paneer Butter Masala","Kadai Paneer","Matar Paneer","Palak Paneer","Aloo Gobi","Baingan Bharta","Bhindi Masala","Malai Kofta","Shahi Paneer","Paneer Tikka Masala","Dum Aloo","Aloo Matar","Paneer Bhurji","Kadhi Pakora","Biryani","Pulao","Jeera Rice","Vegetable Fried Rice","Raita","Salad","Roti","Naan","Paratha","Bhature","Puri","Chapati","Papad"];
+const breakfastList = [{
+  "name": "Aloo Paratha - Stuffed flatbread made with spiced mashed potatoes, served with yogurt or pickle.", 
+  "image": "https://pipingpotcurry.com/wp-content/uploads/2022/11/Aloo-Paratha-Piping-Pot-Curry.jpg",
+  "recipe": "https://pipingpotcurry.com/aloo-paratha/"
+},
+{
+  "name": "Ghobi Paratha - A whole wheat flour dough stuffed with spiced cauliflower filling and cooked until crispy.", 
+  "image": "https://www.subbuskitchen.com/wp-content/uploads/2017/04/Gobi-Paratha_final2.jpg",
+  "recipe": "https://www.subbuskitchen.com/gobi-cauliflower-paratha-recipe/#recipe-video"
+},
+{
+  "name": "Poha - Flattened rice cooked with vegetables and spices, often garnished with peanuts, coriander, and lime.", 
+  "image": "https://www.archanaskitchen.com/images/archanaskitchen/Indian_Breakfast_Tiffins/Indian_Aloo_Batata_Poha_with_Pomegranates_Recipe_Archanas_Kitchen-1.jpg",
+  "recipe": "https://hebbarskitchen.com/poha-recipe-2-ways-kanda-poha-aloo-poha/#:~:text=WAYS%20VIDEO%20RECIPE%3A-,Must%20Read%3A,-sweets%20recipes"
+},
+{
+  "name": "Aloo Tikki - A potato cutlet served with chutney and spices.",
+  "image": "https://sinfullyspicy.com/wp-content/uploads/2023/03/1-1.jpg",
+  "recipe": "https://rajshrifood.com/chole-tikki-chaat-recipe-how-to-make-aloo-tikki-spicy-chole-chaat-recipe-chaat-ideas-indian/#:~:text=Aloo%20Tikki%20Chole,Recipes%20%7C%20Rajshri%20Food"
+},
+{
+  "name": "Uttapam - Thick rice and lentil pancake topped with vegetables, often served with sambar and coconut chutney.", 
+  "image": "https://i.ytimg.com/vi/TJJJyM4ssMk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCa5iy-_l3uJhngu-wL9unGTpsxVA",
+  "recipe": "https://hebbarskitchen.com/vegetable-uttapam-recipe-veg-uttapam/#:~:text=vegetable%20uttapam%20video%20recipe%3A"
+},
+{
+  "name": "Besan Chilla - A savory pancake made with chickpea flour, often filled with vegetables and spices.", 
+  "image": "https://i.ytimg.com/vi/SeW3Ae8JsSg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCTweGcuOWBVW6QxBdoouN78q_4vA",
+  "recipe": "https://hebbarskitchen.com/besan-chilla-recipe-besan-ka-cheela/#:~:text=omelette%20video%20recipe%3A-,Must,-Read%3A"
+},
+{
+  "name": "Vada Pav - A spicy potato fritter served on a bread roll.", 
+  "image": "https://i0.wp.com/thetwincookingproject.net/wp-content/uploads/2020/01/Vada-Pav-with-Dry-Garlic-Chutney-12-scaled.jpg?w=1220&ssl=1",
+  "recipe": "https://hebbarskitchen.com/vada-pav-recipe-make-vada-pav-wada-pav/#:~:text=Video%20Recipe%3A-,Must%20Read%3A,-sweets%20recipes"
+},
+{
+  "name": "Pav Bhaji - A spicy vegetable curry served with buttered bread rolls.", 
+  "image": "https://i.ytimg.com/vi/qMAYG-soxhw/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC7KpqINukdZe2qDS4CbAzb7LZoZA",
+  "recipe": "https://hebbarskitchen.com/easy-mumbai-style-pav-bhaji-recipe/#:~:text=bhaji%20mix%20powder.-,VIDEO,-RECIPE%3A"
+},
+{
+  "name": "Aloo Poori - Deep-fried bread served with spiced mashed potatoes.", 
+  "image": "https://tastedrecipes.com/wp-content/uploads/2020/10/Aloo-Puri-1.jpg",
+  "recipe": "https://hebbarskitchen.com/aloo-puri-recipe-aloo-ki-poori-masala/#:~:text=ALOO%20PURI%20VIDEO-,RECIPE,-%3A"
+},
+{
+  "name": "Paneer Paratha - Stuffed flatbread made with spiced cottage cheese, typically served for breakfast in North India.  ", 
+  "image": "https://pipingpotcurry.com/wp-content/uploads/2022/11/Paneer-Paratha-Piping-Pot-Curry.jpg",
+  "recipe": "https://hebbarskitchen.com/paneer-paratha-recipe/#:~:text=sweets%20recipe%20collection-,paneer,-paratha%20video%20recipe"
+},
+{
+  "name": "Paneer stuffed kulcha is a popular North Indian breakfast dish made with a leavened flatbread stuffed with spiced cottage cheese and cooked until crispy and golden brown.", 
+  "image": "https://snapcook.in/wp-content/uploads/2017/05/stuffed-kulcha-paneer-stuffed-kulcha-paneer-kulcha-sandwich-recipe-main.jpg",
+  "recipe": "https://hebbarskitchen.com/paneer-kulcha-recipe-paneer-kulcha-naan/"
+}]
+
+
+const lunchList = ["Chana Masala", "Rajma Masala", "Aloo Gobi", "Aloo Matar", "Baingan Bharta", "Bhindi Masala", "Palak Paneer", "Matar Paneer", "Paneer Butter Masala", "Kadai Paneer", "Paneer Bhurji", "Mushroom Masala", "Malai Kofta", "Vegetable Kofta Curry", "Navratna Korma", "Jeera Aloo", "Dum Aloo", "Masala Chaas", "Punjabi Samosa", "Chole Bhature", "Pav Bhaji", "Paneer Paratha", "Aloo Paratha", "Gobi Paratha", "Methi Paratha", "Missi Roti", "Lachha Paratha", "Dal Makhani", "Tawa Pulao", "Veg Pulao"];
+const dinnerList = ["Dal Makhani", "Rajma", "Chole", "Paneer Butter Masala", "Kadai Paneer", "Matar Paneer", "Palak Paneer", "Aloo Gobi", "Baingan Bharta", "Bhindi Masala", "Malai Kofta", "Shahi Paneer", "Paneer Tikka Masala", "Dum Aloo", "Aloo Matar", "Paneer Bhurji", "Kadhi Pakora", "Biryani", "Pulao", "Jeera Rice", "Vegetable Fried Rice", "Raita", "Salad", "Roti", "Naan", "Paratha", "Bhature", "Puri", "Chapati", "Papad"];
 
 const breakfastButton = document.getElementById("breakfast-button");
 const lunchButton = document.getElementById("lunch-button");
 const dinnerButton = document.getElementById("dinner-button");
 const foodName = document.getElementById("food-name");
 const foodImage = document.getElementById("food-image");
+const foodRecipe = document.getElementById("food-recipe");
+const foodYoutube = document.getElementById("food-youtube");
+
 
 
 function getRandomItem(list) {
@@ -16,18 +75,20 @@ function getRandomItem(list) {
 
 breakfastButton.addEventListener("click", () => {
   const selectedFood = getRandomItem(breakfastList);
-  foodName.textContent = selectedFood;
-  foodImage.src = `images/${selectedFood.toLowerCase()}.jpg`;
+  foodName.textContent = selectedFood["name"];
+  foodImage.src = selectedFood["image"];
+  foodRecipe.href = selectedFood["recipe"];
 });
 
 lunchButton.addEventListener("click", () => {
   const selectedFood = getRandomItem(lunchList);
   foodName.textContent = selectedFood;
-  foodImage.src = `images/${selectedFood.toLowerCase()}.jpg`;
+  foodImage.src = selectedFood["image"];
 });
 
 dinnerButton.addEventListener("click", () => {
   const selectedFood = getRandomItem(dinnerList);
   foodName.textContent = selectedFood;
-  foodImage.src = `images/${selectedFood.toLowerCase()}.jpg`;
+  foodImage.src = selectedFood["image"];
 });
+
